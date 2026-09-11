54993e6e3758cba28ee28fcc3a008a2ab9435dd6
Hamdullah <124809346+Hamdullah91@users.noreply.github.com>
2026-09-12 02:59:15 +0500

    fix: restore precision-first SEC relation extractor

--- src/nlp/relation_extraction/__init__.py
+++ src/nlp/relation_extraction/__init__.py
@@ -1,4 +1,4 @@
-from .enhanced_extractor import RelationExtractor
+from .extractor import RelationExtractor
 from .models import RelationCandidate
 
 __all__ = ["RelationCandidate", "RelationExtractor"]
